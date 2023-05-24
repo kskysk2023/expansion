d62d78d791499fadcf5d073e199debf55dd0673d
a b <byouyamada@gmail.com>
2023-05-24 21:14:03 +0900

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
+import { DialogRef } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private dialog : MatDialog){}
   title = 'expansion';
+  openErrorDialog(errorMessage: string): void{
+    const dialogRef = this.dialog.open(ErrorDialogComponent, {
+      width: '400px',
+      data: errorMessage 
+    });
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    const reader: FileReader = new FileReader();
+  
+    reader.onload = (e: any) => {
+      const fileContent = e.target.result;
+      if(!fileContent || !(e.target instanceof HTMLInputElement)){
+        console.log("読み込みエラー");
+        this.openErrorDialog("読み込みエラー");
+      }
+
+      if(file.name.endsWith(".csv")){
+        console.log("csvを読み込んだ" + file.name);
+      }
+      else if (file.name.endsWith(".MEM")){
+        console.log("MEMを読み込んだ" + file.name);
+      }
+    };
+    reader.readAsText(file);
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
