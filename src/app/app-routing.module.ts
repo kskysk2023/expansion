210d7f6d84a51299878d57fd305f5c2880cb907d
a b <byouyamada@gmail.com>
2023-06-18 22:51:53 +0900

    ボタンを追加しサーバーへメッセージを送るように

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrepareComponent } from './prepare/prepare.component';
-import { SettingComponent } from './setting/setting.component';
 import { ResultComponent } from './result/result.component';
 
 const routes: Routes = [
--- src/app/prepare/prepare.component.ts
+++ src/app/prepare/prepare.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-
 import { Bind } from '../setting/setting.component';
-import { CompComponent } from '../comp/comp.component';
+import { CompService } from '../comp.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-prepare',
@@ -19,12 +19,18 @@ export class PrepareComponent {
     piston : {I: "×", Q: "×"},
     rupture : {I: "×", Q: "×"}
   }
-  constructor(public compService: CompComponent){}
+  constructor(public compService: CompService, private http : HttpClient){}
   onSelectedChanged() {
     const compression = this.bind.compression;
     const shock = Object.values(this.bind.shock).filter(value => value && value !== "×");
     const rupture = Object.values(this.bind.rupture).filter(value => value && value !== "×");
     const piston = Object.values(this.bind.piston).filter(value => value && value !== "×");
+  }
 
+  onButtonClick(){
+    const data = { message : "push"};
+    this.http.post("/buttonClicked", data, {headers: { 'Content-Type' : 'application/json' }}).subscribe(response => {
+      console.log(response)
+    })
   }
 }
